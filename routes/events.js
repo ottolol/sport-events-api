38711b852af18aeb4eb984e06164ef5d921df446
Aleksey Sveshnikov <alexsveshnik@gmail.com>
2025-06-05 12:47:16 +0300

    Create events.js

---
+++ routes/events.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const eventController = require("../controllers/eventController");
+const { ensureAdmin } = require("../middleware/authMiddleware");
+
+router.get("/", eventController.getAllEvents);
+router.post("/", ensureAdmin, eventController.createEvent);
+router.put("/:id", ensureAdmin, eventController.updateEvent);
+
+module.exports = router;
